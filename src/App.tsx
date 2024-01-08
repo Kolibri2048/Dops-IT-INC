253e12ca7477613c21b91eed9ff32af01a9c67a7
Kolibri2048 <blak_fog@mail.ru>
2024-01-08 07:01:28 +0100

    dop 04 work whit different styles, childre, usefull content

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,72 @@
-import React, {RefObject, useEffect, useRef, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Input} from "./Components/Input";
-import Button from "./Components/Button";
+import {SuperButton} from "./components/SuperButton";
+import s from './components/superButton.module.css'
+import {SuperTodolist} from "./components/SuperTodolist";
 
-type TodosType = {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-}
 
 function App() {
+    const tasks = [
+        { id: 1, title: "HTML&CSS", isDone: true },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false }
+    ]
 
-    const [todos, setTodos] = useState<TodosType[]>([])
-    // const [newTitle, setNewTitle] = useState('')
-    const inputRef = useRef<HTMLInputElement>(null)
-
-    const fetchFoo = () => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(json => setTodos(json))
-    }
-
-    useEffect(() => {
-        fetchFoo()
-    }, []) // Если просто [] догда выпполниться один раз. Если есть useState либо же два и более то выполниться столько раз сколько стэйт поменялся
-
-    console.log(todos)
-
-    const ShowHandler = () => {
-        fetchFoo()
-    }
-
-    const hideHandler = () => {
-        setTodos([])
-    }
-
-    const addNewTask = () => {
-        if(inputRef.current){
-            const newTask = {
-                userId: 666,
-                id: todos.length + 1,
-                title: inputRef.current.value,
-                completed: false
-            }
-            setTodos([newTask, ...todos])
-            inputRef.current.value = ''
-            // setNewTitle('')
-        }
-
-
-    }
     return (
         <div>
-            <div>
-                <button onClick={ShowHandler}>Show todos</button>
-                <button onClick={hideHandler}>Hide todos</button>
-            </div>
+            {/*<SuperButton*/}
+            {/*    onClick={()=>{}}*/}
+            {/*    name={'BLUE BUTTON'}*/}
+            {/*    className={s.stylesForSuperButton}*/}
+            {/*/>*/}
 
-            <div>
-                <Input
-                    inputRef={inputRef}
-                    // newTitle={newTitle}
-                    //    setNewTitle={setNewTitle}
-                />
-                <Button onclick={addNewTask} name={'+'}/>
-            </div>
-            <ul>
-                {todos.map(el => {
-                    return (
-                        <li>
-                            <span>{el.id} - </span>
-                            <span>{el.title}</span>
-                            <input type={"checkbox"} checked={el.completed}/>
-                        </li>
-                    )
-                })}
-            </ul>
+            {/*<SuperButton*/}
+            {/*    onClick={()=>{}}*/}
+            {/*    //name={''}*/}
+            {/*    className={s.stylesForSuperButton}*/}
+            {/*>BLUE BUTTON</SuperButton>*/}
+
+            {/*<SuperButton onClick={() => {}} color={'blue'}>BLUE BUTTON</SuperButton>*/}
+            {/*<SuperButton onClick={() => {}} color={'red'} disabled> RED BUTTON</SuperButton>*/}
+            {/*<SuperButton onClick={() => {}} > DEFAULT BUTTON</SuperButton>*/}
+
+            <SuperTodolist tasks={tasks}>
+                <SuperButton onClick={() => {}} color={'blue'}>BLUE BUTTON</SuperButton>
+                <SuperButton onClick={() => {}} color={'red'} disabled> RED BUTTON</SuperButton>
+                <SuperButton onClick={() => {}} > DEFAULT BUTTON</SuperButton>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <input type="text"/>
+            </SuperTodolist>
+            <SuperTodolist tasks={tasks}>
+                <SuperButton onClick={() => {}} color={'blue'}>BLUE BUTTON</SuperButton>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+            </SuperTodolist>
+            <SuperTodolist tasks={tasks}>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <p>ggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+                <input type="text"/>
+                <input type="text"/>
+            </SuperTodolist>
+
         </div>
     );
 }
 
-export default App;
 
+export default App;
